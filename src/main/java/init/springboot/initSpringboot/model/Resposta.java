b845915424805720c307ac309e9b286d9608edb0
Deyvis <deyvisrf@gmail.com>
2020-06-25 14:47:09 -0300

    install dependency jpa and h2

--- src/main/java/init/springboot/initSpringboot/model/Resposta.java
+++ src/main/java/init/springboot/initSpringboot/model/Resposta.java
@@ -1,13 +1,20 @@
 package init.springboot.initSpringboot.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Resposta {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
 
